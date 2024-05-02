afb7ac982fdc5ef0c2e55e8fe63a02f213ceb9be
Bhavya Sri <bhavyasri2897@gmail.com>
2024-05-02 19:52:57 +0530

    shopping react ui

---
+++ src/shoppingfolder/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="footerSection">
+      <div class="bannerBox">
+        <img src="assets/footer.png" alt="footer"/>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
